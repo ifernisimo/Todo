c64ece6c21b7677950cfc7cace50d46b9802ef13
ifernisimo <47306095+ifernisimo@users.noreply.github.com>
2020-09-28 16:22:26 +0300

    Deleted general reducer

--- src/store/redux-store.js
+++ src/store/redux-store.js
@@ -1,12 +1,10 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunkMiddleware from "redux-thunk";
-import generalReducer from "./reducers/general-reducer";
 import todoReducer from "./reducers/todo-reducer";
 import { reducer as formReducer } from "redux-form";
 import { createLogger } from "redux-logger";
 
 let reducers = combineReducers({
-  general: generalReducer,
   todo: todoReducer,
   form: formReducer,
 });
